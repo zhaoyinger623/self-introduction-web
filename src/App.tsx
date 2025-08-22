8574f0a1b44e89727c857603edb73b38e457dfbe
yingyue.zhao <yingyue.zhao@ximalaya.com>
2025-08-22 15:42:56 +0800

    feat: github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import AppRouter from "./router/Router";
 import "./App.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/self-introduction-web">
       <AppRouter />
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
