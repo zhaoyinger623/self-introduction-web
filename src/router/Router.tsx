0e46751ed8460e9b8d28e3220e1e1e681f59f94e
赵莹玥 <yingyue.zhao@ximalaya.com>
2026-05-08 10:44:17 +0800

    Updates

--- src/router/Router.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Suspense } from "react";
-import { useRoutes } from "react-router-dom";
-import { routes } from "./routerConfig";
-
-const AppRouter: React.FC = () => {
-  return <Suspense fallback={<div></div>}>{useRoutes(routes)}</Suspense>;
-};
-
-export default AppRouter;
